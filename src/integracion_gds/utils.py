e7deea7e21df0ae893cf8c8b977adb52d9b9d643
Juan Urrego <juancho088@gmail.com>
2023-02-09 10:57:29 +0000

    Creacion de DockerFile y Dockecompose

--- src/integracion_gds/utils.py
+++ src/integracion_gds/utils.py
@@ -18,7 +18,7 @@ def millis_a_datetime(millis):
     return datetime.datetime.fromtimestamp(millis/1000.0)
 
 def broker_host():
-    return os.getenv('BROKER_HOST', default="localhost")
+    return os.getenv(PULSAR_ENV, default="localhost")
 
 def consultar_schema_registry(topico: str) -> dict:
     json_registry = requests.get(f'http://{broker_host()}:8080/admin/v2/schemas/{topico}/schema').json()
