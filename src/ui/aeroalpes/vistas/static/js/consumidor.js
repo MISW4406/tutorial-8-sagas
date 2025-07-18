653e0e9467d0ba0a07dae2c413ced6496d2e93e3
Luis Fernando Santa Ospina <l.santa@uniandes.edu.co>
2025-07-18 12:33:30 +0000

    replace gitpod to github codespaces

--- src/ui/aeroalpes/vistas/static/js/consumidor.js
+++ src/ui/aeroalpes/vistas/static/js/consumidor.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
   const messages = document.getElementById("mensajes");    
-  const websocket = new WebSocket("wss://5678-misw4406-tutorial5cqrse-0pton2wd2wp.ws-eu83.gitpod.io/");
+  const websocket = new WebSocket("wss://5678-misw4406-tutorial5cqrse-0pton2wd2wp.ws-eu831.app.github.dev/");
   
   websocket.onmessage = ({ data }) => {
     const message = document.createElement("li");
